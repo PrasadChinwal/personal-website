082214407226587713256c65ba0cf15f44682a39
ChinwalPrasad <pchin3@uis.edu>
2021-07-10 14:53:37 -0500

    Added NProgress for loading indicator

--- components/postbody.js
+++ components/postbody.js
@@ -1,12 +1,8 @@
-import markdownStyles from './markdown-styles.module.css'
 
 export default function PostBody({ content }) {
-  return (
-    <div className="line-numbers shadow-lg rounded-lg p-5 ">
-        <div
-            className="prose"
-            dangerouslySetInnerHTML={{ __html: content }}
-        />
-    </div>
-  )
+    return (
+        <div className="line-numbers shadow-lg rounded-lg p-5 ">
+            <div  className="prose" dangerouslySetInnerHTML={{ __html: content }}/>
+        </div>
+    )
 }
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,13 @@
 import '../styles/globals.css'
 import 'tailwindcss/tailwind.css'
+import "nprogress/nprogress.css";
+import Router from 'next/router';
+import NProgress from 'nprogress'; //nprogress module
+
+// Binding events.
+Router.events.on('routeChangeStart', () => NProgress.start());
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
